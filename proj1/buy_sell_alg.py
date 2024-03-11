60c5178baac684c5cbc0bad419b2acb77dc28916
sebsk <34949127+SebSK3@users.noreply.github.com>
2024-03-11 10:52:14 +0000

    Add option to start from quantity not only money

--- proj1/buy_sell_alg.py
+++ proj1/buy_sell_alg.py
@@ -5,19 +5,24 @@ class Position(Enum):
     LONG = 1
     SHORT = -1
 
-def buy_sell_alg(df, macd, signal, starting_money=1000):
+def buy_sell_alg(df, macd, signal, starting_money=1000, starting_quantity=0):
     money = starting_money
-    quantity = 0
-    position = Position.NONE
+    quantity = starting_quantity
+    if money != 0:
+        position = Position.SHORT
+        quantity = 0
+    elif starting_quantity != 0:
+        position = Position.LONG
+        money = 0
     for i in range(1, len(df)):
-        if macd.iloc[i] > signal.iloc[i] and macd.iloc[i - 1] <= signal.iloc[i - 1] and position != Position.LONG:
+        if macd.iloc[i] > signal.iloc[i] and position != Position.LONG:
             # Kupno
             position = Position.LONG
             buy_price = df['Zamkniecie'].iloc[i]
             quantity = money / buy_price
             df.at[df.index[i], 'Kupno'] = buy_price
             money = 0
-        elif macd.iloc[i] < signal.iloc[i] and macd.iloc[i - 1] >= signal.iloc[i - 1] and position != Position.SHORT:
+        elif macd.iloc[i] < signal.iloc[i] and position != Position.SHORT:
             # Sprzedaż
             position = Position.SHORT
             sell_price = df['Zamkniecie'].iloc[i]
